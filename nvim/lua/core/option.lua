42b436111fcbb9b0a015a75435b19a2718aa6f98
Nexxo <fabiolongofabio@gmail.com>
2026-01-24 17:06:03 +0100

    aled

---
+++ nvim/lua/core/option.lua
@@ -0,0 +1,11 @@
+vim.opt.scrolloff = 18
+
+vim.cmd([[
+  autocmd TermOpen * tnoremap <buffer> <Esc> <C-\><C-n>
+]])
+
+vim.opt.clipboard = "unnamedplus"
+vim.g.mapleader = " " 
+vim.o.number = true 
+
+
